bf136754fc6a932ba3947e8d26d537b3aae70e67
nnFn <44121977+nnfn@users.noreply.github.com>
2018-12-31 00:43:30 +0900

    Update HappyNewYear2019.py

--- HappyNewYear2019.py
+++ HappyNewYear2019.py
@@ -3,7 +3,7 @@ from datetime import datetime
 a = datetime.now().strftime("%Y/%m/%d")
 print(a)
 
-if str(a) == "2018/01/01":
+if str(a) == "201/01/01":
     for i in range(0,2019):
         print("happy New year!")
 
